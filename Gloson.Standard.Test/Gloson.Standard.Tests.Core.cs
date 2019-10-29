3e5acc3f0084eedbad9b2ff939a589a95df6a904
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2019-10-29 17:36:40 +0300

    Tests declared

---
+++ Gloson.Standard.Test/Gloson.Standard.Tests.Core.cs
@@ -0,0 +1,22 @@
+// https://gigi.nullneuron.net/gigilabs/data-driven-tests-with-nunit/
+// https://github.com/nunit/docs/wiki/TestCaseSource-Attribute
+
+using System;
+using NUnit.Framework;
+
+namespace Tests {
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public class Tests {
+    [SetUp]
+    public void Setup() {
+    }
+
+    [Test]
+    public void Test1() {
+      Assert.Pass();
+    }
+  }
+}
