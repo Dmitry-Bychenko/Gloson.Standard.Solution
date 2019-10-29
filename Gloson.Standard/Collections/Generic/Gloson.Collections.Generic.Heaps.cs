e1f44ace2339bb376d510b5d1be2002a4541c1be
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2019-10-29 10:58:05 +0300

    File Structure changes

--- Gloson.Standard/Collections/Generic/Gloson.Collections.Generic.Heaps.cs
+++ Gloson.Standard/Collections/Generic/Gloson.Collections.Generic.Heaps.cs
@@ -14,6 +14,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace Gloson.Collections.Generic {
+
   //-------------------------------------------------------------------------------------------------------------------
   //
   /// <summary>
