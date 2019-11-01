c6cdef56be82162bc500bbaa6038f66fe6f5e303
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2019-11-01 11:42:48 +0300

    Luhn

---
+++ Gloson.Standard/ComponentModel/DataAnnotations/CheckSums/Library/Gloson.ComponentModel.DataAnnotations.CheckSums.Library.Luhn.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gloson.ComponentModel.DataAnnotations.CheckSums.Library {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Luhn Check Sum
+  /// </summary>
+  /// <see cref="https://en.wikipedia.org/wiki/Luhn_algorithm"/>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public sealed class CheckSumLuhn : CheckSumBase<int> {
+    #region Algorithm
+
+    /// <summary>
+    /// Perform computations
+    /// </summary>
+    protected override void Perform() {
+      ControlDigit = -1;
+      IsValid = false;
+      int oddSum = 0;
+      int evenSum = 0;
+      bool isEven = true;
+
+      for (int i = 0; i < Sequence.Count; ++i) {
+        int d = Sequence[i];
+
+        if (d < 0 || d > 9)
+          return;
+
+        if (i == Sequence.Count - 1) {
+          IsValid = d == (10 - (isEven ? oddSum : evenSum)) % 10; 
+        }
+
+        int modified = d * 2 < 10 ? d * 2 : d * 2 - 9;
+
+        if (isEven) {
+          oddSum += d;
+          evenSum += modified;
+        }
+        else {
+          oddSum += modified;
+          evenSum += d;
+        }
+
+        isEven = !isEven;
+        oddSum %= 10;
+        evenSum %= 10;
+      }
+      
+      ControlDigit = (10 - (isEven ? oddSum : evenSum)) % 10;
+    }
+
+    #endregion Algorithm
+
+    #region Create
+
+    /// <summary>
+    /// Standard constructor
+    /// </summary>
+    public CheckSumLuhn(IEnumerable<int> sequence)
+      : base(sequence) { }
+
+    #endregion Create 
+  }
+}
--- Gloson.Standard/Gloson.cs
+++ Gloson.Standard/Gloson.cs
@@ -5,17 +5,16 @@
 //
 // https://stackoverflow.com/questions/49572079/net-core-dependency-injection-backwards-compatibility-with-net-framework
 //
-// 
+// HttpClient
+//
+// 1. Parsing (Tokens)
 // 2. Trie<T>
 // 3. Combinatorics generators
-// 4. Special Functions (Li, Ci, ...Sh, ArTh,...)
-// 5. Statistics Distributions
 // 6. Git
 // 7. Stash
 // 8. Uri ReadLines etc. like File
 // 9. Version
 //
-// HttpClient
 //
 //---------------------------------------------------------------------------------------------------------------------
 
