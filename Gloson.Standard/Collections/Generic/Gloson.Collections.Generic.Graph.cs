7d8e6ce5278558395b37b7e11eb8e0ac307de9fa
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2019-11-05 14:09:16 +0300

    File Lock

---
+++ Gloson.Standard/Collections/Generic/Gloson.Collections.Generic.Graph.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gloson.Collections.Generic {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Graph Options
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  [Flags]
+  public enum GraphOptions {
+    /// <summary>
+    /// 
+    /// </summary>
+    None = 0,
+    /// <summary>
+    /// 
+    /// </summary>
+    Undirected = 1,
+    /// <summary>
+    /// 
+    /// </summary>
+    Loops = 2,
+    /// <summary>
+    /// 
+    /// </summary>
+    MultiEdges = 4,
+    /// <summary>
+    /// 
+    /// </summary>
+    Acyclic = 8,
+  } 
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Graph
+  /// </summary>
+  /// <typeparam name="V">Value Type associated with Vertex</typeparam>
+  /// <typeparam name="E">Value Type associated with Edge</typeparam>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public sealed partial class Graph<V, E> {
+    #region Inner classes
+
+    /// <summary>
+    /// Vertex
+    /// </summary>
+    public sealed class Vertex {
+      #region Private Data
+
+      private HashSet<Edge> m_InEdges = new HashSet<Edge>();
+
+      private HashSet<Edge> m_OutEdges = new HashSet<Edge>();
+
+      #endregion Private Data
+
+      #region Create
+
+      /// <summary>
+      /// Standard constructor
+      /// </summary>
+      /// <param name="graph">Parent Graph</param>
+      /// <param name="value">Value associated with the Vertex</param>
+      public Vertex(Graph<V, E> graph, V value) {
+        if (null == graph)
+          throw new ArgumentNullException(nameof(graph));
+
+        Graph = graph;
+        Value = value;
+
+        if (!Graph.CoreAddVertex(this)) {
+          Graph = null;
+
+          throw new ArgumentException("The vertex can't be added to the graph", nameof(graph));
+        }
+      }
+
+      /// <summary>
+      /// Standard constructor
+      /// </summary>
+      /// <param name="graph">Parent Graph</param>
+      public Vertex(Graph<V, E> graph) : this(graph, default) { }
+
+      #endregion Create
+
+      #region Public
+
+      /// <summary>
+      /// Connect To
+      /// </summary>
+      public Edge ConnectTo(Vertex to, E value) => new Edge(this, to, value);
+
+      /// <summary>
+      /// Connect To
+      /// </summary>
+      public Edge ConnectTo(Vertex to) => new Edge(this, to);
+
+      /// <summary>
+      /// Connect From
+      /// </summary>
+      public Edge ConnectFrom(Vertex from, E value) => new Edge(from, this, value);
+
+      /// <summary>
+      /// Connect From
+      /// </summary>
+      public Edge ConnectFrom(Vertex from) => new Edge(from, this);
+
+      /// <summary>
+      /// Value
+      /// </summary>
+      public V Value { get; set; }
+
+      /// <summary>
+      /// Graph
+      /// </summary>
+      public Graph<V, E> Graph { get; internal set; }
+
+      /// <summary>
+      /// In Edges
+      /// </summary>
+      public IEnumerable<Edge> InEdges => m_InEdges;
+
+      /// <summary>
+      /// Out Edges
+      /// </summary>
+      public IEnumerable<Edge> OutEdges => m_OutEdges;
+
+      #endregion Public
+    }
+
+    /// <summary>
+    /// Edge
+    /// </summary>
+    public sealed class Edge {
+      #region Private Data
+
+      private E m_Value;
+
+      private Edge m_Twin;
+
+      #endregion Private Data
+
+      #region Algorithm
+      #endregion Algorithm
+
+      #region Create
+
+      private Edge(Vertex from, Vertex to, E value, bool check) {
+        To = to;
+        From = from;
+        Value = value;
+      }
+
+      /// <summary>
+      /// Standard Constructor
+      /// </summary>
+      /// <param name="from">From</param>
+      /// <param name="to">To</param>
+      /// <param name="value">Associated value</param>
+      public Edge(Vertex from, Vertex to, E value) {
+        if (null == from)
+          throw new ArgumentNullException(nameof(from));
+        else if (null == to)
+          throw new ArgumentNullException(nameof(to));
+        else if (from.Graph != to.Graph)
+          throw new ArgumentException("From and To vertexes belong to different graphs.", nameof(to));
+
+        if (!from.Graph.IsEdgeAllowed(from, to))
+          throw new ArgumentException("Edge is not allowed", nameof(to));
+
+        To = to;
+        From = from;
+        Value = value;
+
+        // Correct; create create twin
+        if (Graph.Options.HasFlag(GraphOptions.Undirected)) {
+          m_Twin = new Edge(to, from, value, true);
+
+          m_Twin.m_Twin = this;
+        }
+      }
+
+      /// <summary>
+      /// Standard Constructor
+      /// </summary>
+      /// <param name="from">From</param>
+      /// <param name="to">To</param>
+      public Edge(Vertex from, Vertex to) : this(from, to, default) { }
+
+      #endregion Create
+
+      #region Public
+
+      /// <summary>
+      /// From Vertex
+      /// </summary>
+      public Vertex From { get; }
+
+      /// <summary>
+      /// To Vertex
+      /// </summary>
+      public Vertex To { get; }
+
+      /// <summary>
+      /// Graph
+      /// </summary>
+      public Graph<V, E> Graph  => null != From
+        ? From.Graph
+        : null != To ? To.Graph
+        : null;
+
+      /// <summary>
+      /// Value
+      /// </summary>
+      public E Value { 
+        get {
+          return m_Value;
+        } 
+        set {
+          m_Value = value;
+
+          if (null != m_Twin)
+            m_Twin.m_Value = value;
+        } 
+      }
+
+      #endregion Public
+    }
+
+    #endregion Inner classes
+
+    #region Private Data
+
+    private HashSet<Vertex> m_Vertexes = new HashSet<Vertex>();
+
+    #endregion Private Data
+
+    #region Algorithm
+
+    // Add Vertex (always possible)
+    private bool CoreAddVertex(Vertex value) {
+      return null == value
+        ? false
+        : m_Vertexes.Add(value);
+    }
+
+    // If Edge allowed
+    private bool IsEdgeAllowed(Vertex from, Vertex to) {
+      if (null == from)
+        return false;
+      else if (null == to)
+        return false;
+      else if (from.Graph != to.Graph)
+        return false;
+
+      if (from == to && !Options.HasFlag(GraphOptions.Loops))
+        return false;
+
+      if (!Options.HasFlag(GraphOptions.MultiEdges))
+        if (from.OutEdges.Any(edge => edge.To == to) ||
+            to.InEdges.Any(edge => edge.From == from))
+          return false;
+
+      //TODO: Implement me! Acyclic test
+      if (Options.HasFlag(GraphOptions.Acyclic)) {
+
+      }
+
+      return true;
+    }
+
+    #endregion Algorithm
+
+    #region Create
+
+    /// <summary>
+    /// Standard constructor
+    /// </summary>
+    public Graph(GraphOptions options) 
+      : base() {
+
+      Options = options;
+    }
+
+    /// <summary>
+    /// Standard constructor
+    /// </summary>
+    public Graph() : this(GraphOptions.None) { }
+
+    #endregion Create
+
+    #region Public
+
+    /// <summary>
+    /// Options
+    /// </summary>
+    public GraphOptions Options { get; }
+
+    /// <summary>
+    /// Vertexes
+    /// </summary>
+    public IEnumerable<Vertex> Vertexes => m_Vertexes;
+
+    /// <summary>
+    /// Edges
+    /// </summary>
+    public IEnumerable<Edge> Edges => m_Vertexes
+      .SelectMany(vertex => vertex.OutEdges);
+
+    #endregion Public
+  }
+}
---
+++ Gloson.Standard/IO/Gloson.IO.FileLock.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Gloson.IO {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// File Lock
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public sealed class FileLock : IDisposable {
+    #region Private Data
+
+    private FileStream m_Stream;
+
+    #endregion Private Data
+
+    #region Create
+
+    /// <summary>
+    /// Standard Constructor
+    /// </summary>
+    /// <param name="fileName">File Name</param>
+    public FileLock(string fileName) {
+      if (null == fileName)
+        throw new ArgumentNullException(nameof(fileName));
+
+      m_Stream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.None); 
+
+      FileName = fileName;
+    }
+
+    #endregion Create
+
+    #region Public
+
+    /// <summary>
+    /// File Name locked
+    /// </summary>
+    public string FileName { get; }
+
+    #endregion Public
+
+    #region IDisposable
+
+    /// <summary>
+    /// Is Disposed
+    /// </summary>
+    public bool IsDisposed => m_Stream != null;
+
+    /// <summary>
+    /// Dispose
+    /// </summary>
+    /// <param name="disposing"></param>
+    private void Dispose(bool disposing) {
+      if (disposing) {
+        if (IsDisposed) {
+          m_Stream.Dispose();
+        }
+      }
+
+      m_Stream = null;
+    }
+
+    /// <summary>
+    /// Standard Dispose
+    /// </summary>
+    public void Dispose() {
+      Dispose(true);
+    }
+
+    #endregion IDisposable
+  }
+
+}
