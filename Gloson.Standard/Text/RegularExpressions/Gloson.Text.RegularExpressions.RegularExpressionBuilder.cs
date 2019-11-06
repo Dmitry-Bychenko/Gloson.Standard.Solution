8bd725e09827dec768223a9a882db0598eb9bff0
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2019-11-06 10:32:09 +0300

    Regex Options pattern

--- Gloson.Standard/Text/RegularExpressions/Gloson.Text.RegularExpressions.RegularExpressionBuilder.cs
+++ Gloson.Standard/Text/RegularExpressions/Gloson.Text.RegularExpressions.RegularExpressionBuilder.cs
@@ -52,6 +52,65 @@ namespace Gloson.Text.RegularExpressions {
     /// <param name="value">wildcard</param>
     /// <param name="manyJoker">many joker, usually '*'</param>
     /// <param name="oneJoker">many joker, usually '?'</param>
+    /// <param name="escape">escape symbol</param>
+    /// <returns>Regular expression pattern</returns>
+    public static string FromWildCardPattern(string value, char manyJoker, char oneJoker, char escape) {
+      if (null == value)
+        throw new ArgumentNullException(nameof(value));
+
+      StringBuilder sb = new StringBuilder();
+
+      double from = 0;
+      double to = 0;
+
+      for (int i = 0; i < value.Length; ++i) {
+        char c = value[i];
+
+        if (c == escape) {
+          if (from > 0 || to > 0)
+            sb.Append(Multiplicator(".", from, to));
+
+          from = 0;
+          to = 0;
+
+          i += 1;
+
+          if (i < value.Length) {
+            c = value[i];
+            sb.Append(Regex.Escape(c.ToString()));
+          }
+
+          continue;
+        }
+
+        if (c == manyJoker)
+          to = double.PositiveInfinity;
+        else if (c == oneJoker) {
+          from += 1;
+          to += 1;
+        }
+        else {
+          if (from > 0 || to > 0)
+            sb.Append(Multiplicator(".", from, to));
+
+          from = 0;
+          to = 0;
+
+          sb.Append(Regex.Escape(c.ToString()));
+        }
+      }
+
+      sb.Append(Multiplicator(".", from, to));
+
+      return sb.ToString();
+    }
+
+    /// <summary>
+    /// Pattern from wildcard
+    /// </summary>
+    /// <param name="value">wildcard</param>
+    /// <param name="manyJoker">many joker, usually '*'</param>
+    /// <param name="oneJoker">many joker, usually '?'</param>
     /// <returns>Regular expression pattern</returns>
     public static string FromWildCardPattern(string value, char manyJoker, char oneJoker) {
       if (null == value)
@@ -98,6 +157,21 @@ namespace Gloson.Text.RegularExpressions {
     /// <param name="value">wildcard</param>
     /// <param name="manyJoker">many joker, usually '*'</param>
     /// <param name="oneJoker">many joker, usually '?'</param>
+    /// <param name="escape">escape</param>
+    /// <returns>Regular expression pattern</returns>
+    public static string FromWildCardAnchoredPattern(string value, char manyJoker, char oneJoker, char escape) {
+      if (null == value)
+        throw new ArgumentNullException(nameof(value));
+
+      return $"^{FromWildCardPattern(value, manyJoker, oneJoker, escape)}$";
+    }
+
+    /// <summary>
+    /// Pattern from wildcard
+    /// </summary>
+    /// <param name="value">wildcard</param>
+    /// <param name="manyJoker">many joker, usually '*'</param>
+    /// <param name="oneJoker">many joker, usually '?'</param>
     /// <returns>Regular expression pattern</returns>
     public static string FromWildCardAnchoredPattern(string value, char manyJoker, char oneJoker) {
       if (null == value)
@@ -112,6 +186,53 @@ namespace Gloson.Text.RegularExpressions {
     /// <param name="value">wildcard</param>
     /// <returns>Regular expression pattern</returns>
     public static string FromWildCardAnchoredPattern(string value) => FromWildCardAnchoredPattern(value, '*', '?');
+
+    /// <summary>
+    /// From options pattern: abc[XY[Z]]def where abcdef, abcXYdef, abcXYZdef are valid
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static string FromOptionsPattern(string value) {
+      if (null == value)
+        throw new ArgumentNullException(nameof(value));
+
+      StringBuilder sb = new StringBuilder(value.Length * 2 + 8);
+
+      int bracketsCount = 0;
+
+      foreach (char ch in value) {
+        if (ch == '[') {
+          sb.Append("(?:");
+          bracketsCount += 1;
+        }
+        else if (ch == ']') {
+          sb.Append(")?");
+          bracketsCount -= 1;
+
+          if (bracketsCount < 0)
+            throw new FormatException("Too many ']' symbols");
+        }
+        else
+          sb.Append(Regex.Escape(ch.ToString()));
+      }
+
+      if (bracketsCount > 0)
+        throw new FormatException("Too many '[' symbols");
+
+      return sb.ToString();
+    }
+
+    /// <summary>
+    /// From options pattern: abc[XY[Z]]def where abcdef, abcXYdef, abcXYZdef are valid
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static string FromOptionsAnchoredPattern(string value) {
+      if (null == value)
+        throw new ArgumentNullException(nameof(value));
+
+      return $"^{FromOptionsPattern(value)}$";
+    }
 
     #endregion Public
   }
