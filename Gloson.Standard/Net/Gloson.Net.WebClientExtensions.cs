8104a1f3a4d2fc3c529c12f7e54509c604b9a414
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2019-11-11 16:12:54 +0300

    WebClient

---
+++ Gloson.Standard/Net/Gloson.Net.WebClientExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Json;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Gloson.Net {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Web Client extensions
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static class WebClientExtensions {
+    #region Public
+
+    /// <summary>
+    /// With Default credentials
+    /// </summary>
+    public static WebClient WithDefaultCredentials(this WebClient client) {
+      if (null == client)
+        throw new ArgumentNullException(nameof(client));
+
+      client.Credentials = CredentialCache.DefaultCredentials;
+
+      return client;
+    }
+
+    /// <summary>
+    /// Read String with encoding
+    /// </summary>
+    public static string ReadString(this WebClient client, string address, Encoding encoding) {
+      if (null == client)
+        throw new ArgumentNullException(nameof(client));
+
+      byte[] data = client.DownloadData(address);
+
+      return encoding.GetString(data);
+    }
+
+    /// <summary>
+    /// Read Json
+    /// </summary>
+    public static JsonValue ReadJson(this WebClient client, string address, Encoding encoding) {
+      if (null == client)
+        throw new ArgumentNullException(nameof(client));
+
+      return JsonValue.Parse(ReadString(client, address, encoding));
+    }
+
+    /// <summary>
+    /// Read XML
+    /// </summary>
+    public static XDocument ReadXml(this WebClient client, string address, Encoding encoding) {
+      if (null == client)
+        throw new ArgumentNullException(nameof(client));
+
+      return XDocument.Parse(ReadString(client, address, encoding));
+    }
+
+    #endregion Public
+
+  }
+}
