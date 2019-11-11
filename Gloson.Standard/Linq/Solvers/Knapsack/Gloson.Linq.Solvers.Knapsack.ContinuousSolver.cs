56b9a10d6ecfefc31d9870fea2db39afa1404028
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2019-11-11 15:48:38 +0300

    Json / XML

---
+++ Gloson.Standard/Linq/Solvers/Knapsack/Gloson.Linq.Solvers.Knapsack.ContinuousSolver.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gloson.Linq.Solvers.Knapsack {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Continuous Knapsack solver
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static partial class KnapsackSolver {
+    #region Internal classes
+
+    /// <summary>
+    /// Knapsack continuous solution
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class KnapsackContinuousSolution<T>
+      : IReadOnlyList<T> {
+
+      #region Private Data
+
+      private List<T> m_Items = new List<T>();
+
+      private List<int> m_Indexes = new List<int>();
+
+      private List<double> m_Parts = new List<double>();
+
+      #endregion Private Data
+
+      #region Algorithm
+
+      internal void Add(T item, int index, double part, double weight, double value) {
+        m_Items.Add(item);
+        m_Indexes.Add(index);
+        m_Parts.Add(part);
+
+        Value += part * value;
+        Weight += part * weight;
+      }
+
+      #endregion Algorithm
+
+      #region Create
+
+      internal KnapsackContinuousSolution(double capacity) {
+        Capacity = capacity;
+      }
+
+      #endregion Create
+
+      #region Public
+
+      /// <summary>
+      /// Initial capacity
+      /// </summary>
+      public double Capacity { get; }
+
+      /// <summary>
+      /// Total Value of the solution
+      /// </summary>
+      public double Value { get; private set; }
+
+      /// <summary>
+      /// Total Weight of the solution
+      /// </summary>
+      public double Weight { get; private set; }
+
+      /// <summary>
+      /// Indexes of the original items to select
+      /// </summary>
+      public IReadOnlyList<int> Indexes => m_Indexes;
+
+      /// <summary>
+      /// Selected items
+      /// </summary>
+      public IReadOnlyList<T> Items => m_Items;
+
+      /// <summary>
+      /// Parts of items (0..1]
+      /// </summary>
+      public IReadOnlyList<double> Parts => m_Parts;
+
+      /// <summary>
+      /// Debug Information
+      /// </summary>
+      public override string ToString() {
+        if (Items.Count <= 0)
+          return $"Capacity {Capacity} : Empty Solution";
+
+        return $"Capacity {Capacity} : {Items.Count} items with {Value} value and {Weight} weight ([{string.Join(", ", Indexes)}] indexes).";
+      }
+
+      #endregion Public
+
+      #region IReadOnlyList<T>
+
+      /// <summary>
+      /// Count
+      /// </summary>
+      public int Count => Items.Count;
+
+      /// <summary>
+      /// Indexer
+      /// </summary>
+      public T this[int index] => Items[index];
+
+      /// <summary>
+      /// Enumerator
+      /// </summary>
+      public IEnumerator<T> GetEnumerator() => Items.GetEnumerator();
+
+      /// <summary>
+      /// Enumerator
+      /// </summary>
+      IEnumerator IEnumerable.GetEnumerator() => Items.GetEnumerator();
+
+      #endregion IReadOnlyList<T>
+    }
+
+    #endregion Internal classes
+
+    #region Public
+
+    /// <summary>
+    /// Knapsack Zero Or One Solution
+    /// </summary>
+    /// <param name="source">Possible items to take</param>
+    /// <param name="capacity">Knapsack capacity</param>
+    /// <param name="weight">Item's weight</param>
+    /// <param name="value">Item's value</param>
+    /// <returns></returns>
+    public static KnapsackContinuousSolution<T> KnapsackSolveForCpntinuous<T>(
+      this IEnumerable<T> source,
+           double capacity,
+           Func<T, double> weight,
+           Func<T, double> value) {
+
+      if (ReferenceEquals(null, source))
+        throw new ArgumentNullException(nameof(source));
+      else if (ReferenceEquals(null, weight))
+        throw new ArgumentNullException(nameof(weight));
+      else if (ReferenceEquals(null, value))
+        throw new ArgumentNullException(nameof(value));
+
+      var data = source
+        .Select((item, idx) => (
+           item: item,
+           weight: weight(item),
+           value: value(item),
+           index: idx))
+        .Where(item => item.weight <= capacity)
+        .Where(item => item.value > 0 || item.weight < 0)
+        .OrderBy(item => item.weight >= 0)
+        .ThenByDescending(item => item.value / item.weight)
+        .ToList();
+
+      KnapsackContinuousSolution<T> solution = new KnapsackContinuousSolution<T>(capacity);
+
+      for (int i = 0; i < data.Count; ++i) {
+        var item = data[i];
+
+        if (capacity - item.weight >= 0) {
+          // completely
+          capacity -= item.weight;
+
+          solution.Add(item.item, item.index, 1.0, item.weight, item.value);
+        }
+        else if (capacity > 0) {
+          // partially
+          double part = item.weight / capacity;
+
+          solution.Add(item.item, item.index, part, item.weight, item.value);
+
+          break;
+        }
+      }
+
+      return solution;
+    }
+
+    #endregion Public
+  }
+}
---
+++ Gloson.Standard/Net/Http/Gloson.Net.Http.HttpClientExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Json;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Gloson.Net.Http {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Http Client Extensions
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static partial class HttpClientExtensions {
+    #region Public
+
+    /// <summary>
+    /// Read as Json
+    /// </summary>
+    public static async Task<JsonValue> JsonAsync(this HttpClient client, string address) {
+      if (null == client)
+        throw new ArgumentNullException(nameof(client));
+
+      var respond = await client.GetAsync(address);
+
+      respond.EnsureSuccessStatusCode();
+
+      var text = await respond.Content.ReadAsStringAsync();
+
+      return JsonValue.Parse(text);
+    }
+
+    /// <summary>
+    /// Read as XML
+    /// </summary>
+    public static async Task<XDocument> XmlAsync(this HttpClient client, string address) {
+      if (null == client)
+        throw new ArgumentNullException(nameof(client));
+
+      var respond = await client.GetAsync(address);
+
+      respond.EnsureSuccessStatusCode();
+
+      var text = await respond.Content.ReadAsStringAsync();
+
+      return XDocument.Parse(text);
+    }
+
+    #endregion Public
+  }
+}
