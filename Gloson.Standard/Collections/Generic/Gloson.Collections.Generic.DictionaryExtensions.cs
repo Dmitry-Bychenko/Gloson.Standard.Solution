314e7a31622ea48446839acdfb42a64661c8ffb4
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2019-11-15 16:49:40 +0300

    Equality Comparer

---
+++ Gloson.Standard/Collections/Generic/Gloson.Collections.Generic.DictionaryExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gloson.Collections.Generic {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// IDictionary extensions
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static partial class DictionaryExtensions {
+    #region Public
+
+    /// <summary>
+    /// Try Add Or Update
+    /// </summary>
+    /// <typeparam name="K">Key</typeparam>
+    /// <typeparam name="V">Value</typeparam>
+    /// <param name="dictionary">Dictionary</param>
+    /// <param name="key">Key</param>
+    /// <param name="value">Value</param>
+    /// <param name="tie">Tie break</param>
+    /// <returns>true if value added; false if value updated</returns>
+    public static bool TryAddOrUpdate<K, V>(
+      this IDictionary<K, V> dictionary, 
+           K key,
+           V value,
+           Func<K, V, V> tie) {
+
+      if (null == dictionary)
+        throw new ArgumentNullException(nameof(dictionary));
+
+      if (dictionary.TryGetValue(key, out var prior)) {
+        if (tie != null)
+          value = tie(key, prior);
+
+        dictionary[key] = value;
+
+        return false;
+      }
+      else
+        dictionary.Add(key, value);
+
+      return true;
+    }
+
+    /// <summary>
+    /// Try Add Or Update
+    /// </summary>
+    /// <typeparam name="K">Key</typeparam>
+    /// <typeparam name="V">Value</typeparam>
+    /// <param name="dictionary">Dictionary</param>
+    /// <param name="key">Key</param>
+    /// <param name="value">Value</param>
+    /// <returns>true if value added; false if value updated</returns>
+    public static bool TryAddOrUpdate<K, V>(
+      this IDictionary<K, V> dictionary,
+           K key,
+           V value) => TryAddOrUpdate(dictionary, key, value, null);
+
+    #endregion Public
+  }
+
+}
---
+++ Gloson.Standard/Collections/Generic/Gloson.Collections.Generic.EqualityComparers.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gloson.Collections.Generic {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Equality Comparer Builder
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static partial class EqualityComparerBuilder {
+    #region Internal Classes
+
+    private sealed class EqualityComparerFunc<T> : IEqualityComparer<T> {
+      private Func<T, T, bool> m_Equals;
+      private Func<T, int> m_Hash;
+
+      public EqualityComparerFunc(Func<T, T, bool> equals, Func<T, int> hash) {
+        m_Equals = equals;
+        m_Hash = hash;
+      }
+
+      public bool Equals(T x, T y) {
+        if (ReferenceEquals(x, y))
+          return true;
+        else if (ReferenceEquals(null, y))
+          return false;
+        else if (ReferenceEquals(x, null))
+          return false;
+
+        return m_Equals(x, y);
+      }
+
+      public int GetHashCode(T obj) {
+        if (ReferenceEquals(null, obj))
+          return 0;
+
+        return m_Hash(obj);
+      }
+    }
+
+    #endregion Internal Classes
+
+    #region Public
+
+    /// <summary>
+    /// From Funcs
+    /// </summary>
+    /// <param name="equals">Equals</param>
+    /// <param name="hash">Hash</param>
+    /// <returns></returns>
+    public static IEqualityComparer<T> FromFunc<T>(Func<T, T, bool> equals, Func<T, int> hash) {
+      if (null == equals)
+        throw new ArgumentNullException(nameof(equals));
+      else if (null == hash)
+        throw new ArgumentNullException(nameof(hash));
+
+      return new EqualityComparerFunc<T>(equals, hash);
+    }
+
+    #endregion Public
+  }
+
+}
