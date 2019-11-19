5e1024c9a2c48a44bcdfe2a3cc496674819d7864
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2019-11-19 16:16:51 +0300

    Matrix (basic algorithms)

---
+++ Gloson.Standard/Numerics/Matrices/Gloson.Numerics.Matrices.Matrix.cs
@@ -0,0 +1,562 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Gloson.Numerics.Matrices {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Real Value Matrix 
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public sealed class Matrix
+    : ICloneable,
+      IEquatable<Matrix>,
+      IFormattable, 
+      IEnumerable<double> {
+
+    #region Private Data
+
+    // Items
+    internal double[][] m_Items;
+
+    #endregion Private Data
+
+    #region Algorithm 
+    #endregion Algorithm
+
+    #region Create
+
+    // Empty Constructor
+    private Matrix() { }
+
+    /// <summary>
+    /// Standard Constructor
+    /// </summary>
+    /// <param name="lines">Lines</param>
+    /// <param name="columns">Columns</param>
+    public Matrix(int lines, int columns)
+      : this() {
+
+      if (lines <= 0)
+        throw new ArgumentOutOfRangeException(nameof(lines));
+      else if (columns <= 0)
+        throw new ArgumentOutOfRangeException(nameof(columns));
+
+      m_Items = Enumerable
+        .Range(0, lines)
+        .Select(_ => new double[columns])
+        .ToArray();
+    }
+
+    /// <summary>
+    /// Standard Constructor (square matrix)
+    /// </summary>
+    /// <param name="size">Size</param>
+    public Matrix(int size) : this(size, size) { }
+
+    /// <summary>
+    /// Union matgrix
+    /// </summary>
+    /// <param name="size">Size</param>
+    public static Matrix Union(int size) {
+      Matrix result = new Matrix(size);
+
+      for (int i = result.LineCount - 1; i >= 0; --i)
+        result.m_Items[i][i] = 1;
+
+      return result;
+    }
+
+    /// <summary>
+    /// Create Matrix
+    /// </summary>
+    /// <param name="lines">Lines</param>
+    /// <param name="columns">Columns</param>
+    /// <param name="createItem">Item at line and column</param>
+    /// <returns></returns>
+    public static Matrix Create(int lines, int columns, Func<int, int, double> createItem) {
+      if (lines <= 0)
+        throw new ArgumentOutOfRangeException(nameof(lines));
+      else if (columns <= 0)
+        throw new ArgumentOutOfRangeException(nameof(columns));
+      else if (null == createItem)
+        throw new ArgumentNullException(nameof(createItem));
+
+      Matrix result = new Matrix(lines, columns);
+
+      for (int r = 0; r < lines; ++r)
+        for (int c = 0; c < columns; ++c)
+          result.m_Items[r][c] = createItem(r, c);
+
+      return result;
+    }
+
+    /// <summary>
+    /// Create Square Matrix
+    /// </summary>
+    /// <param name="size">Size</param>
+    /// <param name="createItem">Item at line and column</param>
+    /// <returns></returns>
+    public static Matrix Create(int size, Func<int, int, double> createItem) => Create(size, size, createItem);
+
+    #endregion Create
+
+    #region Public
+
+    #region General
+
+    /// <summary>
+    /// Perform a func and create a new matrix
+    /// </summary>
+    /// <param name="func">func: value, line, column returns a new value</param>
+    /// <returns>New Matrix</returns>
+    public Matrix Perform(Func<double, int, int, double> func) {
+      if (null == func)
+        throw new ArgumentNullException(nameof(func));
+
+      Matrix result = new Matrix(LineCount, ColumnCount);
+
+      for (int r = 0; r < m_Items.Length; ++r)
+        for (int c = 0; c < m_Items[r].Length; ++c)
+          result.m_Items[r][c] = func(m_Items[r][c], r, c);
+
+      return result;
+    }
+
+    /// <summary>
+    /// Perform a func and create a new matrix
+    /// </summary>
+    /// <param name="func">func: line, column returns a new value</param>
+    /// <returns>New Matrix</returns>
+    public Matrix Perform(Func<int, int, double> func) {
+      if (null == func)
+        throw new ArgumentNullException(nameof(func));
+
+      Matrix result = new Matrix(LineCount, ColumnCount);
+
+      for (int r = 0; r < m_Items.Length; ++r)
+        for (int c = 0; c < m_Items[r].Length; ++c)
+          result.m_Items[r][c] = func(r, c);
+
+      return result;
+    }
+
+    /// <summary>
+    /// Perform a func and create a new matrix
+    /// </summary>
+    /// <param name="func">func: value returns a new value</param>
+    /// <returns>New Matrix</returns>
+    public Matrix Perform(Func<double, double> func) {
+      if (null == func)
+        throw new ArgumentNullException(nameof(func));
+
+      Matrix result = new Matrix(LineCount, ColumnCount);
+
+      for (int r = 0; r < m_Items.Length; ++r)
+        for (int c = 0; c < m_Items[r].Length; ++c)
+          result.m_Items[r][c] = func(m_Items[r][c]);
+
+      return result;
+    }
+
+    /// <summary>
+    /// Lines
+    /// </summary>
+    public int LineCount => m_Items.Length;
+
+    /// <summary>
+    /// Columns
+    /// </summary>
+    public int ColumnCount => m_Items[0].Length;
+
+    /// <summary>
+    /// Item Count
+    /// </summary>
+    public int ItemCount => m_Items.Length * m_Items[0].Length;
+
+    /// <summary>
+    /// Lines
+    /// </summary>
+    public IEnumerable<double[]> Lines {
+      get {
+        foreach (var line in m_Items)
+          yield return line.ToArray();
+      }
+    }
+
+    /// <summary>
+    /// Lines
+    /// </summary>
+    public IEnumerable<double[]> Columns {
+      get {
+        int N = ColumnCount;
+
+        for (int i = 0; i < N; ++i) {
+          double[] result = new double[m_Items.Length];
+
+          for (int j = 0; j < result.Length; ++j)
+            result[j] = m_Items[i][j];
+
+          yield return result;
+        }
+      }
+    }
+
+    /// <summary>
+    /// Items
+    /// </summary>
+    public IEnumerable<double> Items {
+      get {
+        foreach (var line in m_Items)
+          foreach (var item in line)
+            yield return item;
+      }
+    }
+
+    /// <summary>
+    /// Cell
+    /// </summary>
+    /// <param name="line">Line</param>
+    /// <param name="column">Column</param>
+    public double Cell(int line, int column) {
+      return m_Items[line][column];
+    }
+
+    /// <summary>
+    /// Cell
+    /// </summary>
+    /// <param name="line">Line</param>
+    /// <param name="column">Column</param>
+    public double this[int line, int column] => Cell(line, column);
+
+    #endregion General
+
+    #region Standard
+
+    /// <summary>
+    /// Transpose
+    /// </summary>
+    public Matrix Transpose() {
+      Matrix result = new Matrix(ColumnCount, LineCount);
+
+      for (int r = result.m_Items.Length - 1; r >= 0; --r)
+        for (int c = result.m_Items[0].Length - 1; c >= 0; --c)
+          result.m_Items[r][c] = m_Items[c][r];
+
+      return result;
+    }
+
+    #endregion Standard
+
+    #endregion Public
+
+    #region Operators
+
+    #region Comparison
+
+    /// <summary>
+    /// Equals
+    /// </summary>
+    public static bool operator ==(Matrix left, Matrix right) {
+      if (ReferenceEquals(left, right))
+        return true;
+      else if (ReferenceEquals(null, right))
+        return false;
+      else if (ReferenceEquals(null, left))
+        return false;
+
+      return left.Equals(right);
+    }
+
+    /// <summary>
+    /// Not Equals
+    /// </summary>
+    public static bool operator !=(Matrix left, Matrix right) {
+      if (ReferenceEquals(left, right))
+        return false;
+      else if (ReferenceEquals(null, right))
+        return true;
+      else if (ReferenceEquals(null, left))
+        return true;
+
+      return !left.Equals(right);
+    }
+
+    #endregion Comparison
+
+    #region Arithmetics
+
+    /// <summary>
+    /// Unary +
+    /// </summary>
+    public static Matrix operator + (Matrix value) {
+      if (null == value)
+        throw new ArgumentNullException(nameof(value));
+
+      return value;
+    }
+
+    /// <summary>
+    /// Unary -
+    /// </summary>
+    public static Matrix operator - (Matrix value) {
+      if (null == value)
+        throw new ArgumentNullException(nameof(value));
+
+      Matrix result = value.Clone();
+
+      foreach (double[] line in result.m_Items) 
+        for (int i = line.Length; i >= 0; --i)
+          line[i] = -line[i];
+
+      return result;
+    }
+
+    /// <summary>
+    /// Multiplication by number
+    /// </summary>
+    public static Matrix operator * (Matrix matrix, double value) {
+      if (null == matrix)
+        throw new ArgumentNullException(nameof(matrix));
+
+      if (value == 1)
+        return matrix;
+
+      Matrix result = matrix.Clone();
+
+      foreach (double[] line in result.m_Items)
+        for (int i = line.Length; i >= 0; --i)
+          line[i] = line[i] * value;
+
+      return result;
+    }
+
+    /// <summary>
+    /// Multiplication by number
+    /// </summary>
+    public static Matrix operator * (double value, Matrix matrix) => matrix * value;
+
+    /// <summary>
+    /// Division by number
+    /// </summary>
+    public static Matrix operator / (Matrix matrix, double value) {
+      if (null == matrix)
+        throw new ArgumentNullException(nameof(matrix));
+
+      if (value == 1)
+        return matrix;
+
+      Matrix result = matrix.Clone();
+
+      foreach (double[] line in result.m_Items)
+        for (int i = line.Length; i >= 0; --i)
+          line[i] = line[i] / value;
+
+      return result;
+    }
+
+    /// <summary>
+    /// Matrix Addition
+    /// </summary>
+    public static Matrix operator + (Matrix left, Matrix right) {
+      if (ReferenceEquals(null, left))
+        throw new ArgumentNullException(nameof(left));
+      else if (ReferenceEquals(null, right))
+        throw new ArgumentNullException(nameof(right));
+
+      if (left.LineCount != right.LineCount)
+        throw new ArgumentException($"Right matrix must have {left.LineCount} lines, actual {right.LineCount}", nameof(right));
+      else if (left.ColumnCount != right.ColumnCount)
+        throw new ArgumentException($"Right matrix must have {left.ColumnCount} columns, actual {right.ColumnCount}", nameof(right));
+
+      Matrix result = new Matrix(left.LineCount, left.LineCount);
+
+      for (int r = right.LineCount - 1; r >= 0; --r)
+        for (int c = right.ColumnCount - 1; c >= 0; --c)
+          result.m_Items[r][c] = left.m_Items[r][c] + right.m_Items[r][c];
+
+      return result;
+    }
+
+    /// <summary>
+    /// Matrix Subtractions
+    /// </summary>
+    public static Matrix operator - (Matrix left, Matrix right) {
+      if (ReferenceEquals(null, left))
+        throw new ArgumentNullException(nameof(left));
+      else if (ReferenceEquals(null, right))
+        throw new ArgumentNullException(nameof(right));
+
+      if (left.LineCount != right.LineCount)
+        throw new ArgumentException($"Right matrix must have {left.LineCount} lines, actual {right.LineCount}", nameof(right));
+      else if (left.ColumnCount != right.ColumnCount)
+        throw new ArgumentException($"Right matrix must have {left.ColumnCount} columns, actual {right.ColumnCount}", nameof(right));
+
+      Matrix result = new Matrix(left.LineCount, left.LineCount);
+
+      for (int r = right.LineCount - 1; r >= 0; --r)
+        for (int c = right.ColumnCount - 1; c >= 0; --c)
+          result.m_Items[r][c] = left.m_Items[r][c] - right.m_Items[r][c];
+
+      return result;
+    }
+
+    /// <summary>
+    /// Matrix Mutiplication
+    /// </summary>
+    public static Matrix operator * (Matrix left, Matrix right) {
+      if (ReferenceEquals(null, left))
+        throw new ArgumentNullException(nameof(left));
+      else if (ReferenceEquals(null, right))
+        throw new ArgumentNullException(nameof(right));
+
+      if (left.ColumnCount != right.LineCount)
+        throw new ArgumentException($"Right matrix must have {left.ColumnCount} liness, actual {right.LineCount}", nameof(right));
+
+      Matrix result = new Matrix(left.LineCount, right.ColumnCount);
+
+      for (int r = result.m_Items.Length - 1; r >= 0; --r)
+        for (int c = result.m_Items[0].Length - 1; c >= 0; --c) {
+          double v = 0.0;
+
+          for (int i = right.LineCount - 1; i >= 0; --i)
+            v += left.m_Items[r][i] * right.m_Items[i][c];
+
+          result.m_Items[r][c] = v;
+        }
+
+          return result;
+    }
+
+    #endregion Arithmetics
+
+    #endregion Operators
+
+    #region ICloneable
+
+    /// <summary>
+    /// Deep copy
+    /// </summary>
+    public Matrix Clone() {
+      Matrix copy = new Matrix();
+
+      copy.m_Items = m_Items
+        .Select(line => line.ToArray())
+        .ToArray();
+
+      return copy;
+    }
+
+    /// <summary>
+    /// Deep Copy
+    /// </summary>
+    object ICloneable.Clone() => this.Clone();
+
+    #endregion ICloneable
+
+    #region IEquatable<Matrix>
+
+    /// <summary>
+    /// Equals
+    /// </summary>
+    public bool Equals(Matrix other, double tolerance) {
+      if (tolerance < 0)
+        throw new ArgumentOutOfRangeException(nameof(tolerance));
+
+      if (ReferenceEquals(this, other))
+        return true;
+      else if (ReferenceEquals(null, other))
+        return false;
+
+      if (this.ColumnCount != other.ColumnCount || this.LineCount != other.LineCount)
+        return false;
+
+      for (int r = LineCount - 1; r >= 0; --r)
+        for (int c = ColumnCount - 1; c >= 0; --c)
+          if (Math.Abs(m_Items[r][c] - other.m_Items[r][c]) < tolerance)
+            continue; // for double.NaN case or alike
+          else
+            return false;
+
+      return true;
+    }
+
+    /// <summary>
+    /// Equals
+    /// </summary>
+    public bool Equals(Matrix other) {
+      if (ReferenceEquals(this, other))
+        return true;
+      else if (ReferenceEquals(null, other))
+        return false;
+
+      if (this.ColumnCount != other.ColumnCount || this.LineCount != other.LineCount)
+        return false;
+
+      return m_Items
+        .Zip(other.m_Items, (left, right) => left.SequenceEqual(right))
+        .All(item => item);
+    }
+
+    /// <summary>
+    /// Equals
+    /// </summary>
+    public override bool Equals(object obj) => Equals(obj as Matrix);
+
+    /// <summary>
+    /// Hash Code
+    /// </summary>
+    public override int GetHashCode() => unchecked((LineCount << 16) ^ ColumnCount ^ m_Items[0][0].GetHashCode());
+
+    #endregion IEquatable<Matrix>
+
+    #region IFormattable
+
+    /// <summary>
+    /// To String
+    /// </summary>
+    public string ToString(string format, IFormatProvider formatProvider) {
+      if (null == formatProvider)
+        formatProvider = CultureInfo.InvariantCulture;
+
+      if (string.IsNullOrEmpty(format) || "g".Equals(format, StringComparison.OrdinalIgnoreCase))
+        return ToString();
+
+      return string.Join(Environment.NewLine, m_Items
+        .Select(line => string.Join("\t", line.Select(item => item.ToString(format, formatProvider)))));
+    }
+
+    /// <summary>
+    /// To String
+    /// </summary>
+    public override string ToString() {
+      return string.Join(Environment.NewLine, m_Items
+        .Select(line => string.Join("\t", line.Select(item => item.ToString(CultureInfo.InvariantCulture)))));
+    }
+
+    #endregion IFormattable
+
+    #region IEnumerable<double>
+
+    /// <summary>
+    /// Enumerator
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator<double> GetEnumerator() => Items.GetEnumerator();
+
+    /// <summary>
+    /// Enumerator
+    /// </summary>
+    IEnumerator IEnumerable.GetEnumerator() => Items.GetEnumerator();
+
+    #endregion IEnumerable<double>
+
+  }
+}
