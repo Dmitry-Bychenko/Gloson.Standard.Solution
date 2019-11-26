420bf176fb03873da65b0bbd45bfa71e10dc5666
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2019-11-26 14:56:37 +0300

    HLS Colors

---
+++ Gloson.Standard/Drawing/Gloson.Drawing.ColorF.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Gloson.Drawing {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// ColorF (floating point; all components are in [0..1] range)
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public struct ColorF : IEquatable<ColorF> {
+    #region Create
+
+    /// <summary>
+    /// Standard constructor
+    /// </summary>
+    public ColorF(float a, float r, float g, float b) {
+      if (a < 0 || a > 1.0)
+        throw new ArgumentOutOfRangeException(nameof(a));
+      else if (r < 0 || r > 1.0)
+        throw new ArgumentOutOfRangeException(nameof(r));
+      else if (g < 0 || g > 1.0)
+        throw new ArgumentOutOfRangeException(nameof(g));
+      else if (b < 0 || b > 1.0)
+        throw new ArgumentOutOfRangeException(nameof(b));
+
+      A = a;
+      R = r;
+      G = g;
+      B = b;
+    }
+
+    /// <summary>
+    /// Standard constructor
+    /// </summary>
+    public ColorF(float r, float g, float b)
+      : this(0.0f, r, g, b) { }
+
+    /// <summary>
+    /// Standard constructor
+    /// </summary>
+    public ColorF(Color color)
+      : this(color.A / 255.0f, color.R / 255.0f, color.G / 255.0f, color.B / 255.0f) { }
+
+    #endregion Create
+
+    #region Public
+
+    /// <summary>
+    /// A
+    /// </summary>
+    public float A { get; }
+
+    /// <summary>
+    /// Red
+    /// </summary>
+    public float R { get; }
+
+    /// <summary>
+    /// Green
+    /// </summary>
+    public float G { get; }
+
+    /// <summary>
+    /// Blue
+    /// </summary>
+    public float B { get; }
+
+    /// <summary>
+    /// Gray Scale
+    /// </summary>
+    public float GrayScale => 0.299f * R + 0.587f * G + 0.114f * B;
+
+    /// <summary>
+    /// To Gray Scale
+    /// </summary>
+    /// <returns></returns>
+    public ColorF ToGrayScale() {
+      float gs = GrayScale;
+
+      return new ColorF(A, gs, gs, gs);
+    }
+
+    /// <summary>
+    /// To String
+    /// </summary>
+    public override string ToString() {
+      return string.Join("; ",
+        $"A : {A:f3}",
+        $"R : {R:f3}",
+        $"G : {G:f3}",
+        $"B : {B:f3}");
+    }
+
+    #endregion Public
+
+    #region Operators
+
+    #region Cast
+
+    /// <summary>
+    /// To Color
+    /// </summary>
+    public Color ToColor() => Color.FromArgb(
+      (int) (A * 255 + 0.5),
+      (int) (R * 255 + 0.5),
+      (int) (G * 255 + 0.5),
+      (int) (B * 255 + 0.5)
+    );
+
+    /// <summary>
+    /// To Color
+    /// </summary>
+    public static implicit operator Color(ColorF value) => value.ToColor();
+
+    /// <summary>
+    /// From Color
+    /// </summary>
+    public static implicit operator ColorF(Color color) => new ColorF(color);
+
+    #endregion Cast
+
+    #endregion Operators
+
+    #region IEquatable<ColorF>
+
+    /// <summary>
+    /// Equals
+    /// </summary>
+    public bool Equals(ColorF other) {
+      return A == other.A &&
+             R == other.R &&
+             G == other.G &&
+             B == other.B;
+    }
+
+    /// <summary>
+    /// Equals
+    /// </summary>
+    public override bool Equals(object obj) {
+      if (obj is ColorF other)
+        return Equals(other);
+      else
+        return false;
+    }
+
+    /// <summary>
+    /// Get Hash Code
+    /// </summary>
+    public override int GetHashCode() {
+      return A.GetHashCode() ^ 
+             R.GetHashCode() ^ 
+             G.GetHashCode() ^ 
+             B.GetHashCode();
+    }
+
+    #endregion IEquatable<ColorF>
+  }
+}
---
+++ Gloson.Standard/Drawing/Gloson.Drawing.ColorHls.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Gloson.Drawing {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Hue Luminosity Saturation Floating point color
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public struct ColorHlsF : IEquatable<ColorHlsF> {
+    #region Algorithm
+    #endregion Algorithm
+
+    #region Create
+
+    /// <summary>
+    /// Standard Constructor
+    /// </summary>
+    public ColorHlsF(float a, float h, float l, float s) {
+      if (a < 0.0 || a > 1.0)
+        throw new ArgumentOutOfRangeException(nameof(a));
+      else if (h < 0.0 || h > 1.0)
+        throw new ArgumentOutOfRangeException(nameof(h));
+      else if (l < 0.0 || l > 1.0)
+        throw new ArgumentOutOfRangeException(nameof(l));
+      else if (s < 0.0 || s > 1.0)
+        throw new ArgumentOutOfRangeException(nameof(s));
+
+      A = a;
+      H = h;
+      L = l;
+      S = s;
+    }
+
+    /// <summary>
+    /// Standard Constructor
+    /// </summary>
+    public ColorHlsF(float h, float l, float s)
+      : this(0.0f, h, l, s) { }
+
+    #endregion Create
+
+    #region Public
+
+    /// <summary>
+    /// A
+    /// </summary>
+    public float A { get; }
+
+    /// <summary>
+    /// Hue
+    /// </summary>
+    public float H { get; }
+
+    /// <summary>
+    /// Luminosity
+    /// </summary>
+    public float L { get; }
+
+    /// <summary>
+    /// Saturation
+    /// </summary>
+    public float S { get; }
+
+    /// <summary>
+    /// To String
+    /// </summary>
+    public override string ToString() => string.Join("; ",
+        $"A : {A:f3}",
+        $"H : {H:f3}",
+        $"L : {L:f3}",
+        $"S : {S:f3}");
+
+    #endregion Public
+
+    #region IEquatable<ColorHlsF>
+
+    /// <summary>
+    /// Equals
+    /// </summary>
+    public bool Equals(ColorHlsF other) {
+      return A == other.A &&
+             H == other.H &&
+             L == other.L &&
+             S == other.S;
+    }
+
+    /// <summary>
+    /// Equals
+    /// </summary>
+    public override bool Equals(object obj) =>
+      (obj is ColorHlsF other) ? Equals(other) : false;
+
+    /// <summary>
+    /// Get Hash Code
+    /// </summary>
+    public override int GetHashCode() =>
+      A.GetHashCode() ^ H.GetHashCode() ^ L.GetHashCode() ^ S.GetHashCode();
+
+    #endregion IEquatable<ColorHlsF>
+  }
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Hue Luminosity Saturation Floating point color
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public struct ColorHls : IEquatable<ColorHls> {
+    #region Algorithm 
+    #endregion Algorithm
+
+    #region Create
+
+    /// <summary>
+    /// Standard Constructor
+    /// </summary>
+    /// <param name="a">A</param>
+    /// <param name="h">Hue</param>
+    /// <param name="l">Luminosity</param>
+    /// <param name="s">Saturation</param>
+    public ColorHls(int a, int h, int l, int s) {
+      if (a < 0 || a > Byte.MaxValue)
+        throw new ArgumentOutOfRangeException(nameof(a));
+      else if (h < 0 || h > Byte.MaxValue)
+        throw new ArgumentOutOfRangeException(nameof(h));
+      else if (l < 0 || l > Byte.MaxValue)
+        throw new ArgumentOutOfRangeException(nameof(l));
+      else if (s < 0 || s > Byte.MaxValue)
+        throw new ArgumentOutOfRangeException(nameof(s));
+
+      unchecked {
+        A = (byte)a;
+        H = (byte)h;
+        L = (byte)l;
+        S = (byte)s;
+      }
+    }
+
+    /// <summary>
+    /// Standard Constructor
+    /// </summary>
+    /// <param name="h">Hue</param>
+    /// <param name="l">Luminosity</param>
+    /// <param name="s">Saturation</param>
+    public ColorHls(int h, int l, int s) :
+      this(0, h, l, s) { }
+
+    #endregion Create
+
+    #region Public
+
+    /// <summary>
+    /// A
+    /// </summary>
+    public byte A { get; }
+
+    /// <summary>
+    /// H
+    /// </summary>
+    public byte H { get; }
+
+    /// <summary>
+    /// L
+    /// </summary>
+    public byte L { get; }
+
+    /// <summary>
+    /// S
+    /// </summary>
+    public byte S { get; }
+
+    /// <summary>
+    /// To String
+    /// </summary>
+    public override string ToString() => string.Join("; ",
+        $"A : {A,3}",
+        $"H : {H,3}",
+        $"L : {L,3}",
+        $"S : {S,3}");
+
+    #endregion Public
+
+    #region IEquatable<ColorHls>
+
+    /// <summary>
+    /// Equals
+    /// </summary>
+    public bool Equals(ColorHls other) {
+      return A == other.A &&
+             H == other.H &&
+             L == other.L &&
+             S == other.S;
+    }
+
+    /// <summary>
+    /// Equals
+    /// </summary>
+    public override bool Equals(object obj) =>
+      (obj is ColorHlsF other) ? Equals(other) : false;
+
+    /// <summary>
+    /// Get Hash Code
+    /// </summary>
+    public override int GetHashCode() =>
+      unchecked((A << 24) | (H << 16) | (L << 8) | (S));
+
+    #endregion IEquatable<ColorHls>
+  }
+}
