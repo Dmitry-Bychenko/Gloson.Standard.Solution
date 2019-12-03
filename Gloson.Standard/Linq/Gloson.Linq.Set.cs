5a92788c0d2923cbeb43752f0544f285f81ad9a4
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2019-12-03 17:29:19 +0300

    Set Linq

--- Gloson.Standard/Linq/Gloson.Linq.Set.cs
+++ Gloson.Standard/Linq/Gloson.Linq.Set.cs
@@ -91,7 +91,7 @@ namespace Gloson.Linq {
       IntersectWithDuplicates(source, other, null);
 
     /// <summary>
-    /// Intersect with duplicates
+    /// Except with duplicates
     /// </summary>
     public static IEnumerable<T> ExceptWithDuplicates<T>(this IEnumerable<T> source,
                                                               IEnumerable<T> other,
@@ -129,13 +129,13 @@ namespace Gloson.Linq {
     }
 
     /// <summary>
-    /// Intersect with duplicates
+    /// Except with duplicates
     /// </summary>
     public static IEnumerable<T> ExceptWithDuplicates<T>(this IEnumerable<T> source, IEnumerable<T> other) =>
       ExceptWithDuplicates(source, other, null);
 
     /// <summary>
-    /// Intersect with duplicates
+    /// Union with duplicates
     /// </summary>
     public static IEnumerable<T> UnionWithDuplicates<T>(this IEnumerable<T> source,
                                                              IEnumerable<T> other,
@@ -178,11 +178,60 @@ namespace Gloson.Linq {
     }
 
     /// <summary>
-    /// Intersect with duplicates
+    /// Union with duplicates
     /// </summary>
     public static IEnumerable<T> UnionWithDuplicates<T>(this IEnumerable<T> source, IEnumerable<T> other) =>
       UnionWithDuplicates(source, other, null);
-    
+
+    /// <summary>
+    /// Symmetric Except with duplicates
+    /// </summary>
+    public static IEnumerable<T> SymmetricExceptWithDuplicates<T>(this IEnumerable<T> source,
+                                                             IEnumerable<T> other,
+                                                             IEqualityComparer<T> comparer) {
+      if (ReferenceEquals(null, source))
+        throw new ArgumentNullException(nameof(source));
+      else if (ReferenceEquals(null, other))
+        throw new ArgumentNullException(nameof(other));
+
+      if (ReferenceEquals(null, comparer))
+        comparer = EqualityComparer<T>.Default;
+
+      if (ReferenceEquals(null, comparer))
+        throw new ArgumentNullException(nameof(comparer),
+          $"{typeof(T).Name} doesn't provide default IEqualityComparer<{typeof(T).Name}>");
+
+      Dictionary<T, int> dict1 = source
+        .GroupBy(item => item, comparer)
+        .ToDictionary(group => group.Key, group => group.Count());
+
+      Dictionary<T, int> dict2 = other
+        .GroupBy(item => item, comparer)
+        .ToDictionary(group => group.Key, group => group.Count());
+
+      foreach (var pair in dict1) {
+        int v = pair.Value;
+
+        if (dict2.TryGetValue(pair.Key, out int v2)) 
+          v = Math.Min(v, v2);
+
+        for (int i = 0; i < v; ++i)
+          yield return pair.Key;
+      }
+
+      foreach (var pair in dict2) {
+        if (!dict1.ContainsKey(pair.Key))
+          for (int i = 0; i < pair.Value; ++i)
+            yield return pair.Key;
+      }
+    }
+
+    /// <summary>
+    /// Symmetric Except with duplicates
+    /// </summary>
+    public static IEnumerable<T> SymmetricExceptWithDuplicates<T>(this IEnumerable<T> source, IEnumerable<T> other) =>
+      SymmetricExceptWithDuplicates(source, other, null);
+
     #endregion Public
   }
 }
