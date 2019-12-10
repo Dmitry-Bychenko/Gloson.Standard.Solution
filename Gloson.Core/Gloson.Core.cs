3faf8249a8a1d4c06e6bfc50e99ccf986c67467f
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2019-12-10 18:04:19 +0300

    Gloson.Core

---
+++ Gloson.Core/Gloson.Core.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Gloson.Core {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Entry Point
+  /// </summary>
+  //
+  // https://docs.microsoft.com/ru-ru/dotnet/standard/assembly/unloadability
+  // https://docs.microsoft.com/ru-ru/dotnet/standard/assembly/load-unload
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static class GlosonCore {
+  }
+}
