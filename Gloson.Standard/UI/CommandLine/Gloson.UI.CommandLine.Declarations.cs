d73dbf7a8a62a12493ff8ebac2cd287cdb213261
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2019-12-26 16:51:40 +0300

    Declarations

---
+++ Gloson.Standard/UI/CommandLine/Gloson.UI.CommandLine.Declarations.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gloson.UI.CommandLine {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Command Line Type
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public enum CommandLineType {
+    None = 0,
+    Boolean = 1,
+    Integer = 2,
+    FloatingPoint = 3,
+    Date = 4,
+    Time = 5,
+    DateTime = 6,
+    String = 7,
+
+    File = 8,
+    Directory = 9,
+    FileOrDirectory = 10,
+    ExistingFile = 11,
+    ExistingDirectory = 12,
+    ExistingFileOrDirectory = 13
+  }
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Command Line Type Extensions
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static class CommandLineTypeExtensions {
+    #region Public
+
+    /// <summary>
+    /// Wants / Has parameter
+    /// </summary>
+    public static bool WantsValue(CommandLineType kind) {
+      return kind != CommandLineType.None;
+    }
+
+    #endregion Public
+  }
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Command Line Descriptions Options
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  [Flags]
+  public enum CommandLineDescriptorsOptions {
+    None = 0,
+    CaseSensitive = 1,
+    OptionalPrefix = 2,
+  }
+
+}
--- Gloson.Standard/UI/CommandLine/Gloson.UI.CommandLine.Descriptions.cs
+++ Gloson.Standard/UI/CommandLine/Gloson.UI.CommandLine.Descriptions.cs
@@ -9,46 +9,7 @@ using Gloson.Text.RegularExpressions;
 
 namespace Gloson.UI.CommandLine {
 
-  //-------------------------------------------------------------------------------------------------------------------
-  //
-  /// <summary>
-  /// Command Line Type
-  /// </summary>
-  //
-  //-------------------------------------------------------------------------------------------------------------------
 
-  public enum CommandLineType {
-    None = 0,
-    Boolean = 1,
-    Integer = 2,
-    FloatingPoint = 3,
-    Date = 4,
-    Time = 5,
-    DateTime = 6,
-    String = 7,
-
-    File = 8,
-    Directory = 9,
-    FileOrDirectory = 10,
-    ExistingFile = 11,
-    ExistingDirectory = 12,
-    ExistingFileOrDirectory = 13
-  }
-
-  //-------------------------------------------------------------------------------------------------------------------
-  //
-  /// <summary>
-  /// Command Line Descriptions Options
-  /// </summary>
-  //
-  //-------------------------------------------------------------------------------------------------------------------
-
-  [Flags]
-  public enum CommandLineDescriptorsOptions {
-    None = 0,
-    CaseSensitive = 1,
-    OptionalPrefix = 2,
-  }
 
   //-------------------------------------------------------------------------------------------------------------------
   //
