b00b87725279b6209071db761ea83fb339eb87ba
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2020-01-10 15:19:13 +0300

    Configure

---
+++ Gloson.Standard/UI/Dialogs/CommandLine/Gloson.UI.Dialogs.CommandLine.Configure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gloson.UI.Dialogs.CommandLine {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Command Line Configure
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static class CommandLineConfigure {
+    #region Public
+
+    /// <summary>
+    /// Configure
+    /// </summary>
+    public static void Configure() {
+      Dependencies.RegisterService(typeof(IAboutDialog), typeof(AboutDialog));
+      Dependencies.RegisterService(typeof(IUnhandledExceptionDialog), typeof(UnhandledExceptionDialog));
+    }
+
+    #endregion Public
+  }
+}
