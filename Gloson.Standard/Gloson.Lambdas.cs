70ff169ba529f52bcb5bfd3248366f174a7ffbac
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2020-01-16 18:10:25 +0300

    Labmda

---
+++ Gloson.Standard/Gloson.Lambdas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gloson {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Func Extensions
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static class FuncExtensions {
+    #region Public
+
+    /// <summary>
+    /// Pipeline
+    /// </summary>
+    public static Func<T, T> Pipeline<T>(this Func<T, T> first, params Func<T, T>[] next) {
+      if (null == first)
+        throw new ArgumentNullException(nameof(first));
+      else if (null == next)
+        throw new ArgumentNullException(nameof(first));
+
+      if (next.Length <= 0)
+        return first;
+
+      return (T v) => {
+        T r = first(v);
+
+        foreach (var f in next)
+          r = f(r);
+
+        return r;
+      };
+    }
+
+    #endregion Public
+  }
+}
