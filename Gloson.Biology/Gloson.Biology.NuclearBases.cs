2f542c329c8bbb787598d2184e22ee95523fcd7a
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2020-02-05 13:37:34 +0300

    RNA

--- Gloson.Biology/Gloson.Biology.NuclearBases.cs
+++ Gloson.Biology/Gloson.Biology.NuclearBases.cs
@@ -60,6 +60,42 @@ namespace Gloson.Biology {
       }
     }
 
+    /// <summary>
+    /// RNA Complement
+    /// </summary>
+    public static RnaNuclearbase RnaComplement(this DnaNuclearbase value) {
+      switch (value) {
+        case DnaNuclearbase.A:
+          return RnaNuclearbase.U;
+        case DnaNuclearbase.C:
+          return RnaNuclearbase.G;
+        case DnaNuclearbase.G:
+          return RnaNuclearbase.C;
+        case DnaNuclearbase.T:
+          return RnaNuclearbase.A;
+        default:
+          return RnaNuclearbase.A;
+      }
+    }
+
+    /// <summary>
+    /// To Char
+    /// </summary>
+    public static char ToChar(this DnaNuclearbase value) {
+      switch (value) {
+        case DnaNuclearbase.A:
+          return 'A';
+        case DnaNuclearbase.C:
+          return 'C';
+        case DnaNuclearbase.G:
+          return 'G';
+        case DnaNuclearbase.T:
+          return 'T';
+        default:
+          return '?';
+      }
+    }
+
     #endregion Public
   }
 
@@ -173,6 +209,99 @@ namespace Gloson.Biology {
       }
     }
 
+    /// <summary>
+    /// DNA Complement
+    /// </summary>
+    public static DnaNuclearbase DnaComplement(this RnaNuclearbase value) {
+      switch (value) {
+        case RnaNuclearbase.A:
+          return DnaNuclearbase.T;
+        case RnaNuclearbase.C:
+          return DnaNuclearbase.G;
+        case RnaNuclearbase.G:
+          return DnaNuclearbase.C;
+        case RnaNuclearbase.U:
+          return DnaNuclearbase.A;
+        default:
+          return DnaNuclearbase.A;
+      }
+    }
+
+    /// <summary>
+    /// To Char
+    /// </summary>
+    public static char ToChar(this RnaNuclearbase value) {
+      switch (value) {
+        case RnaNuclearbase.A:
+          return 'A';
+        case RnaNuclearbase.C:
+          return 'C';
+        case RnaNuclearbase.G:
+          return 'G';
+        case RnaNuclearbase.U:
+          return 'U';
+        default:
+          return '?';
+      }
+    }
+
     #endregion Public
   }
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// RNA Nuclear base helper
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static class RnaNuclearbaseHelper {
+    #region Public
+
+    /// <summary>
+    /// Try Parse
+    /// </summary>
+    public static bool TryParse(char item, out RnaNuclearbase result) {
+      if (item == 'a' || item == 'A') {
+        result = RnaNuclearbase.A;
+
+        return true;
+      }
+      else if (item == 'c' || item == 'C') {
+        result = RnaNuclearbase.C;
+
+        return true;
+      }
+      else if (item == 'g' || item == 'G') {
+        result = RnaNuclearbase.G;
+
+        return true;
+      }
+      else if (item == 'u' || item == 'U') {
+        result = RnaNuclearbase.U;
+
+        return true;
+      }
+
+      result = RnaNuclearbase.A;
+
+      return false;
+    }
+
+    /// <summary>
+    /// Parse
+    /// </summary>
+    public static RnaNuclearbase Parse(char item) {
+      if (TryParse(item, out var result))
+        return result;
+      else
+        throw new FormatException($"nuclear base {item} is not valid");
+    }
+
+    #endregion Public
+  }
+
+
+
 }
---
+++ Gloson.Biology/Gloson.Biology.Rna.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gloson.Biology {
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// RNA
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public sealed class Rna :
+    IReadOnlyList<RnaNuclearbase>,
+    IEquatable<Rna> {
+
+    #region Private Data
+
+    private List<RnaNuclearbase> m_Items;
+
+    #endregion Private Data
+
+    #region Create
+
+    private Rna() { }
+
+    /// <summary>
+    /// Standard Constructor
+    /// </summary>
+    public Rna(IEnumerable<RnaNuclearbase> sequence) {
+      if (null == sequence)
+        throw new ArgumentNullException(nameof(sequence));
+
+      m_Items = new List<RnaNuclearbase>(sequence);
+    }
+
+    /// <summary>
+    /// Standrad constructor
+    /// </summary>
+    /// <param name="sequence"></param>
+    public Rna(IEnumerable<char> sequence) {
+      if (null == sequence)
+        throw new ArgumentNullException(nameof(sequence));
+
+      m_Items = sequence
+        .Select(item => RnaNuclearbaseHelper.Parse(item))
+        .ToList();
+    }
+
+    /// <summary>
+    /// Standard Constructor
+    /// </summary>
+    public static Rna FromDna(Dna source) {
+      if (null == source)
+        throw new ArgumentNullException(nameof(source));
+
+      Rna result = new Rna();
+
+      result.m_Items = new List<RnaNuclearbase>(source.Count);
+
+      for (int i = 0; i < source.Count; ++i)
+        result.m_Items.Add(source[i].RnaComplement());
+
+      return result;
+    }
+
+    #endregion Create
+
+    #region Public
+
+    /// <summary>
+    /// Items
+    /// </summary>
+    public IReadOnlyList<RnaNuclearbase> Items => m_Items;
+
+    /// <summary>
+    /// To String
+    /// </summary>
+    public override string ToString() => string.Concat(m_Items);
+
+    #endregion Public
+
+    #region Operators
+
+    /// <summary>
+    /// Equals
+    /// </summary>
+    public static bool operator ==(Rna left, Rna right) {
+      if (ReferenceEquals(left, right))
+        return true;
+      else if (ReferenceEquals(left, null) || ReferenceEquals(null, right))
+        return false;
+
+      return left.Equals(right);
+    }
+
+    /// <summary>
+    /// Not Equals
+    /// </summary>
+    public static bool operator !=(Rna left, Rna right) {
+      if (ReferenceEquals(left, right))
+        return false;
+      else if (ReferenceEquals(left, null) || ReferenceEquals(null, right))
+        return true;
+
+      return !left.Equals(right);
+    }
+
+    /// <summary>
+    /// From String
+    /// </summary>
+    public static implicit operator Rna(string value) => new Rna(value);
+
+    #endregion Operators
+
+    #region IReadOnlyList<RnaNuclearbase>
+
+    /// <summary>
+    /// Enumerator
+    /// </summary>
+    public IEnumerator<RnaNuclearbase> GetEnumerator() => m_Items.GetEnumerator();
+
+    /// <summary>
+    /// Typeless Enumerator
+    /// </summary>
+    IEnumerator IEnumerable.GetEnumerator() => m_Items.GetEnumerator();
+
+    /// <summary>
+    /// Count
+    /// </summary>
+    public int Count => m_Items.Count;
+
+    /// <summary>
+    /// Indexer
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public RnaNuclearbase this[int index] => m_Items[index];
+
+    #endregion IReadOnlyList<RnaNuclearbase>
+
+    #region IEquatable<Rna>
+
+    /// <summary>
+    /// Equals
+    /// </summary>
+    public bool Equals(Rna other) {
+      if (ReferenceEquals(this, other))
+        return true;
+      else if (ReferenceEquals(null, other))
+        return false;
+
+      return Enumerable.SequenceEqual(m_Items, other.m_Items);
+    }
+
+    /// <summary>
+    /// Equals
+    /// </summary>
+    public override bool Equals(object obj) => Equals(obj as Rna);
+
+    /// <summary>
+    /// Hash Code
+    /// </summary>
+    public override int GetHashCode() {
+      if (m_Items == null)
+        return 0;
+
+      unchecked {
+        int n = Math.Min(16, m_Items.Count);
+
+        int result = 0;
+
+        for (int i = 0; i < n; ++i)
+          result ^= ((byte)m_Items[i] << (i * 8));
+
+        return result ^ m_Items.Count;
+      }
+    }
+
+    #endregion IEquatable<Rna>
+  }
+}
