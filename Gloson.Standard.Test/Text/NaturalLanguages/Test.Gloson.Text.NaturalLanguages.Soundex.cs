bb1bcdbbc3a02d221fca18a68745da5228674889
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2020-02-12 10:41:00 +0300

    Soundex

---
+++ Gloson.Standard.Test/Text/NaturalLanguages/Test.Gloson.Text.NaturalLanguages.Soundex.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Gloson.Text.NaturalLanguages;
+
+using NUnit.Framework;
+
+namespace Gloson.Standard.Test.Text.NaturalLanguages {
+
+  public class SoundexTest {
+    #region Tests
+
+    public static List<TestCaseData> TestCases() {
+      return new List<TestCaseData>() {
+        new TestCaseData("ammonium").Returns("A555"),
+        new TestCaseData("implementation").Returns("I514"),
+        new TestCaseData("Robert").Returns("R163"),
+        new TestCaseData("Rupert").Returns("R163"),
+        new TestCaseData("Rubin").Returns("R150"),
+
+        new TestCaseData("Ashcraft").Returns("A261"),
+        new TestCaseData("Ashcroft").Returns("A261"),
+        new TestCaseData("Tymczak").Returns("T522"),
+      };
+    }
+
+    [TestCaseSource("TestCases")]
+    //https://ru.wikipedia.org/wiki/Soundex
+    public string TestKnownValues(string value) {
+      string actual = Soundex.Encode(value);
+
+      return actual;
+    }
+
+    #endregion Tests
+  }
+}
---
+++ Gloson.Standard/Text/NaturalLanguages/Gloson.Text.NaturalLanguages.Soundex.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Gloson.Text;
+
+namespace Gloson.Text.NaturalLanguages {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Soundex (English)
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static class Soundex {
+    #region Private Data
+
+    private static Dictionary<char, int> s_Correspondence = new Dictionary<char, int>(CharacterComparer.OrdinalIgnoreCase) {
+      { 'b', 1},
+      { 'f', 1},
+      { 'p', 1},
+      { 'v', 1},
+
+      { 'c', 2},
+      { 'g', 2},
+      { 'j', 2},
+      { 'k', 2},
+      { 'q', 2},
+      { 's', 2},
+      { 'x', 2},
+      { 'z', 2},
+
+      { 'd', 3},
+      { 't', 3},
+
+      { 'l', 4},
+
+      { 'm', 5},
+      { 'n', 5},
+
+      { 'r', 6},
+    };
+
+    #endregion Private Data
+
+    #region Public
+
+    /// <summary>
+    /// Encode To Soundex
+    /// </summary>
+    /// <see cref="https://ru.wikipedia.org/wiki/Soundex"/>
+    public static string Encode(string value) {
+      if (string.IsNullOrEmpty(value))
+        return value;
+
+      int size = 4;
+
+      StringBuilder sb = new StringBuilder();
+
+      foreach (char x in value.Normalize(NormalizationForm.FormD)) {
+        char c = char.ToUpper(x);
+
+        if (c < 'A' || c > 'Z')
+          continue;
+
+        if (sb.Length == 0) {
+          sb.Append(c);
+
+          continue;
+        }
+
+        if (c == 'H' || c == 'W')
+          continue;
+
+        if (s_Correspondence.TryGetValue(c, out int v)) {
+          if (sb[sb.Length - 1] == '0' + v)
+            continue;
+
+          sb.Append(v);
+        }
+        else if (sb[sb.Length - 1] != '0')
+          sb.Append('0');
+
+        if (sb.Length >= size * 2 + 1)
+          break;
+      }
+
+      string result = sb.ToString().Replace("0", "").PadRight(size, '0');
+
+      return result.Length > size
+        ? result.Substring(0, size)
+        : result;
+    }
+
+    #endregion Public
+  }
+
+}
