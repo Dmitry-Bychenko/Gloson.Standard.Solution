9697a266b6d5ed69d60259de61dfab9a06b090e8
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2020-02-18 16:52:02 +0300

    Z-Algorithm

---
+++ Gloson.Standard/Text/Gloson.Text.ZAlgorithm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gloson.Text {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Z-Algorithm
+  /// </summary>
+  /// https://www.geeksforgeeks.org/z-algorithm-linear-time-pattern-searching-algorithm/
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static class ZAlgorithm {
+    #region Public
+
+    /// <summary>
+    /// ZArray from given string
+    /// </summary>
+    public static int[] ZArray(string value) {
+      if (string.IsNullOrEmpty(value))
+        return new int[0];
+
+      int n = value.Length;
+      int L = 0, R = 0;
+
+      int[] result = new int[n];
+
+      result[0] = n;
+
+      for (int i = 1; i < n; ++i) {
+        if (i > R) {
+          L = R = i;
+
+          while (R < n && value[R - L] == value[R])
+            ++R;
+
+          result[i] = R - L;
+          --R;
+        }
+        else {
+          int k = i - L;
+
+          if (result[k] < R - i + 1)
+            result[i] = result[k];
+          else {
+            L = i;
+
+            while (R < n && value[R - L] == value[R])
+              ++R;
+
+            result[i] = R - L;
+            --R;
+          }
+        }
+      }
+
+      return result;
+    }
+
+    #endregion Public
+  }
+}
