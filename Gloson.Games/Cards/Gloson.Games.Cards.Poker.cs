94a4ee1bd080ff0ec362f0685be8a09e6e382e21
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2020-02-20 17:24:54 +0300

    Pokers (start)

---
+++ Gloson.Games/Cards/Gloson.Games.Cards.Poker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+using Gloson.Linq;
+
+namespace Gloson.Games.Cards {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Poker Combinations
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public enum PokerCombination {
+    None = 0,
+    Pair = 1,
+    TwoPairs = 2,
+    Three = 3,
+    Straight = 4,
+    Flush = 5,
+    FullHouse = 6,
+    Four = 7,
+    StraitFlush = 8,
+    RoyalFlush = 9,
+    Poker = 10
+  }
+}
