6f3e65066650cc2e4cc80fe0700efa0f4515a824
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2020-03-13 17:29:11 +0300

    Evaluate

---
+++ Gloson.Standard/Data/Gloson.Data.Evaluate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Gloson.Data {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Evaluate
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static class Evaluate {
+    #region Public
+
+    /// <summary>
+    /// Run 
+    /// </summary>
+    public static T Run<T>(string formula) {
+      using DataTable table = new DataTable();
+
+      return (T)(Convert.ChangeType(table.Compute(formula, null), typeof(T)));
+    }
+
+    #endregion Public
+  }
+}
