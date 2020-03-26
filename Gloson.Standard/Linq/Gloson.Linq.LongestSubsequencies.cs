ab16a3c898a986c10bebcb4eeb304c4082c92497
Bytchenko, Dmitry <Dmitry.Bytchenko@billing.ru>
2020-03-26 13:13:01 +0300

    Longest Common Subsequence

--- Gloson.Standard/Linq/Gloson.Linq.LongestSubsequencies.cs
+++ Gloson.Standard/Linq/Gloson.Linq.LongestSubsequencies.cs
@@ -102,6 +102,85 @@ namespace Gloson.Linq {
     public static T[] LongestNonDecreasingSubsequence<T>(this IEnumerable<T> source, IComparer<T> comparer) =>
       LongestIncreasingSubsequence<T>(source, true, comparer);
 
+    /// <summary>
+    /// Longest Common Subsequence
+    /// </summary>
+    public static T[] LongestCommonSubsequence<T>(this IEnumerable<T> source,
+                                                       IEnumerable<T> target,
+                                                       IEqualityComparer<T> comparer = null) {
+      if (null == source)
+        throw new ArgumentNullException(nameof(source));
+      else if (null == target)
+        throw new ArgumentNullException(nameof(target));
+
+      comparer = (comparer ?? EqualityComparer<T>.Default)
+                           ?? throw new ArgumentNullException(nameof(comparer),
+                                $"Type {typeof(T).Name} doesn't have default equality comparer.");
+
+      T[] left = source.ToArray();
+      T[] right = target.ToArray();
+
+      int[][] data = Enumerable
+        .Range(0, left.Length + 1)
+        .Select(_ => new int[right.Length + 1])
+        .ToArray();
+
+      for (int i = 1; i <= left.Length; ++i)
+        for (int j = 1; j <= right.Length; ++j)
+          if (comparer.Equals(left[i - 1], right[j - 1]))
+            data[i][j] = data[i - 1][j - 1] + 1;
+          else
+            data[i][j] = Math.Max(data[i - 1][j], data[i][j - 1]);
+
+      int y = left.Length;
+      int x = right.Length;
+      int v = data[y][x];
+
+      List<T> result = new List<T>();
+
+      for (; x > 0 && y > 0 && v > 0;) {
+        int dd = data[y - 1][x - 1];
+        int dy = data[y - 1][x];
+        int dx = data[y][x - 1];
+
+        if (dd >= dx) {
+          if (dd >= dy) {
+            x -= 1;
+            y -= 1;
+          }
+          else {
+            y -= 1;
+          }
+        }
+        else if (dy >= dx) {
+          if (dy >= dd) {
+            y -= 1;
+          }
+          else {
+            x -= 1;
+            y -= 1;
+          }
+        }
+        else {
+          x -= 1;
+        }
+
+        int newV = data[y][x];
+
+        if (newV < v) {
+          result.Add(left[y]);
+
+          v = newV;
+        }
+      }
+
+      T[] dataToReturn = result.ToArray();
+
+      Array.Reverse(dataToReturn);
+
+      return dataToReturn;
+    }
+
     #endregion Public
   }
 }
