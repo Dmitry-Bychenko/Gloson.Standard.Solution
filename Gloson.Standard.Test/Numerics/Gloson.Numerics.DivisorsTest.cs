5536e0ae952c46be6c9f98228a7b6e25519826fe
Dmitry.Bychenko <zwerg.coppernickel@gmail.com>
2020-04-03 17:16:20 +0300

    Tests Totient

---
+++ Gloson.Standard.Test/Numerics/Gloson.Numerics.DivisorsTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using NUnit.Framework;
+
+using Gloson.Numerics;
+
+namespace Gloson.Standard.Test.Numerics {
+  public class DivisorsTest {
+    #region Tests
+
+    [TestCase(0, 0)]
+    [TestCase(1, 1)]
+    [TestCase(2, 1)]
+    [TestCase(3, 2)]
+    [TestCase(4, 2)]
+    [TestCase(5, 4)]
+    [TestCase(100, 40)]
+    [TestCase(101, 100)]
+    public void TestValues(int value, int expected) {
+      Assert.IsTrue(Divisors.Totient(value) == expected);
+    }
+
+    #endregion Tests
+  }
+}
