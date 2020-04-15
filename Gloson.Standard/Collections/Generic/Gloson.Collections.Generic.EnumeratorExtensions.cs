fdb4cc5fb6ca9aa3e475efabada959949f22f205
Dmitry.Bychenko <zwerg.coppernickel@gmail.com>
2020-04-15 16:40:37 +0300

    Enumerator Extensions

---
+++ Gloson.Standard/Collections/Generic/Gloson.Collections.Generic.EnumeratorExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Gloson.Collections.Generic {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Enumerator Extensions
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static class EnumeratorExtensions {
+    #region Public
+
+    /// <summary>
+    /// AsEnumerable 
+    /// </summary>
+    public static IEnumerable<T> AsEnumerable<T>(this IEnumerator<T> enumerator) {
+      if (null == enumerator)
+        throw new ArgumentNullException(nameof(enumerator));
+
+      using (enumerator) {
+        while (enumerator.MoveNext())
+          yield return enumerator.Current; 
+      }
+    }
+
+    /// <summary>
+    /// AsEnumerable 
+    /// </summary>
+    public static IEnumerable<T> AsEnumerable<T>(this IEnumerator<T> enumerator, bool keepOpen) {
+      if (null == enumerator)
+        throw new ArgumentNullException(nameof(enumerator));
+
+      if (keepOpen)
+        while (enumerator.MoveNext())
+          yield return enumerator.Current;
+      else
+        using (enumerator) {
+          while (enumerator.MoveNext())
+            yield return enumerator.Current;
+        }
+    }
+
+    #endregion Public
+  }
+}
