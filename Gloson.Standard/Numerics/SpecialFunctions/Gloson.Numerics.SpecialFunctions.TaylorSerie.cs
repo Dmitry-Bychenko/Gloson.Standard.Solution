7c512efeade17b8c2a3380bada793e2a3c225c2c
Dmitry.Bychenko <zwerg.coppernickel@gmail.com>
2020-04-23 14:15:26 +0300

    Taylor serie

---
+++ Gloson.Standard/Numerics/SpecialFunctions/Gloson.Numerics.SpecialFunctions.TaylorSerie.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gloson.Numerics.SpecialFunctions {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Taylor Serie
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static class TaylorSerie {
+    #region Public
+
+    /// <summary>
+    /// Tailor Serie
+    /// </summary>
+    /// <param name="x">Argument</param>
+    /// <param name="next">Next term from index, argument, prior term</param>
+    /// <param name="index">index</param>
+    /// <param name="start">start term</param>
+    /// <returns></returns>
+    public static IEnumerable<double> Serie(double x,
+                                            Func<int, double, double, double> next,
+                                            int index = 0,
+                                            double start = 0.0) {
+      if (null == next)
+        throw new ArgumentNullException(nameof(next));
+
+      double result = start;
+
+      for (int i = index; ; ++i) {
+        result = next(i, x, result);
+
+        yield return result;
+      }
+    }
+
+    #endregion Public
+  }
+}
