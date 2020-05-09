77a791834284471f13042cb6bdb6d83400e93e17
Dmitry.Bychenko <zwerg.coppernickel@gmail.com>
2020-05-09 22:58:34 +0300

    Weeks

--- Gloson.Standard/Gloson.DateTimeExtensions.cs
+++ Gloson.Standard/Gloson.DateTimeExtensions.cs
@@ -46,6 +46,15 @@ namespace Gloson {
     public static DateTime WeekStart(this DateTime date) =>
       WeekStart(date, CultureInfo.CurrentCulture.DateTimeFormat);
 
+    /// <summary>
+    /// Next day of week starting from a iven date
+    /// </summary>
+    /// <param name="fromDate">Date to start from</param>
+    /// <param name="dayOfWeek">Day Of Week</param>
+    /// <returns>The current of future date of day of week</returns>
+    public static DateTime NextDayOfWeek(this DateTime fromDate, DayOfWeek dayOfWeek) =>
+      fromDate.Date.AddDays((7 + (int)dayOfWeek - (int)fromDate.DayOfWeek) % 7);
+
     #endregion Public
   }
 }
