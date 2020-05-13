265e39c106434d7f921dd82408bdff81ffb6e764
Dmitry.Bychenko <zwerg.coppernickel@gmail.com>
2020-05-13 15:11:35 +0300

    Consuming

---
+++ Gloson.Standard/Linq/Gloson.Linq.Consuming.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gloson.Linq {
+
+  public static class ConsumingEnumerables {
+    #region public
+
+    /// <summary>
+    /// Consuming enumeration
+    /// </summary>
+    public static IEnumerable<T> Consuming<T>(this Queue<T> value) {
+      if (null == value)
+        throw new ArgumentNullException(nameof(value));
+
+      while (value.Count > 0)
+        yield return value.Dequeue();
+    }
+
+    /// <summary>
+    /// Consuming enumeration
+    /// </summary>
+    public static IEnumerable<T> Consuming<T>(this Stack<T> value) {
+      if (null == value)
+        throw new ArgumentNullException(nameof(value));
+
+      while (value.Count > 0)
+        yield return value.Pop();
+    }
+
+    #endregion public
+  }
+}
