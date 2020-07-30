90953e28e50a50f00b255d0816831c771a8ca3a2
Dmitry.Bychenko <zwerg.coppernickel@gmail.com>
2020-07-30 15:38:43 +0300

    Compress Chars

---
+++ Gloson.Standard/Text/Gloson.Text.Transformation.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Gloson.Text {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Text Transformations
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static partial class TextTransformation {
+    #region Public
+
+    /// <summary>
+    /// Compress Repeating Characters: 
+    /// "AAAABCBBCDAEEE" -> "A4BCB2CDA3"
+    /// </summary>
+    public static string CompressRepeatingChars(string value) {
+      if (string.IsNullOrEmpty(value))
+        return value;
+
+      StringBuilder sb = new StringBuilder(value.Length);
+      int count = 0;
+      char current = '\0';
+
+      foreach (char c in value) {
+        if (sb.Length <= 0 || current != c) {
+          if (count > 1)
+            sb.Append(count);
+
+          sb.Append(c);
+
+          count = 1;
+          current = c;
+        }
+        else
+          count += 1;
+      }
+
+      if (count > 1)
+        sb.Append(count);
+
+      return sb.ToString();
+    }
+
+    /// <summary>
+    /// Decompress Repeating Characters: 
+    /// "A4BCB2CDA3" -> "AAAABCBBCDAEEE" 
+    /// </summary>
+    public static string DecompressRepeatingChars(string value) {
+      if (string.IsNullOrEmpty(value))
+        return value;
+
+      return Regex.Replace(
+        value,
+       "([^0-9])([0-9]+)",
+        m => new string(m.Groups[1].Value[0], int.Parse(m.Groups[2].Value)));
+    }
+
+    #endregion Public
+  }
+}
