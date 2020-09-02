66c192ea87a124d0c1ee297ca98301aa46cfd056
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2020-09-02 13:52:54 +0300

    Trimmed Distribution Proprortional

--- Gloson.Standard/Numerics/Distributions/Gloson.Numerics.Distributions.Trimmed.cs
+++ Gloson.Standard/Numerics/Distributions/Gloson.Numerics.Distributions.Trimmed.cs
@@ -123,4 +123,123 @@ namespace Gloson.Numerics.Distributions {
 
     #endregion Public
   }
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Trimmed (Original) distribution which now defined in [LeftBorder..RightBorder]
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public sealed class ProportionalTrimContinuousDistribution : ContinuousProbabilityDistribution {
+    #region Private Data
+
+    private readonly double m_CdfLeft;
+
+    private readonly double m_CdfRight;
+
+    private readonly double m_Multiplicator;
+
+    #endregion Private Data
+
+    #region Create
+
+    /// <summary>
+    /// Standard 
+    /// </summary>
+    /// <param name="origin">Original Distribution</param>
+    /// <param name="leftBorder">Left Border</param>
+    /// <param name="rightBorder">Right Border</param>
+    public ProportionalTrimContinuousDistribution(IContinuousProbabilityDistribution origin,
+                                                  double leftBorder,
+                                                  double rightBorder) {
+      Origin = origin ?? throw new ArgumentNullException(nameof(origin));
+
+      if (leftBorder <= rightBorder) {
+        LeftBorder = leftBorder;
+        RightBorder = rightBorder;
+      }
+      else
+        throw new ArgumentOutOfRangeException(nameof(leftBorder), "Empty region");
+
+      m_CdfRight = Origin.Cdf(RightBorder);
+      m_CdfLeft = Origin.Cdf(LeftBorder);
+
+      if (m_CdfRight <= 0)
+        throw new ArgumentOutOfRangeException(nameof(rightBorder), "Zero density region");
+
+      if (leftBorder != rightBorder && (m_CdfRight - m_CdfLeft <= 0))
+        throw new ArgumentOutOfRangeException(nameof(rightBorder), "Zero density region");
+
+      m_Multiplicator = 1.0 / (m_CdfRight - m_CdfLeft);
+    }
+
+    #endregion Create
+
+    #region Public
+
+    /// <summary>
+    /// Original Distribution
+    /// </summary>
+    public IContinuousProbabilityDistribution Origin { get; }
+
+    /// <summary>
+    /// Left Border
+    /// </summary>
+    public double LeftBorder { get; }
+
+    /// <summary>
+    /// Right Border
+    /// </summary>
+    public double RightBorder { get; }
+
+    /// <summary>
+    /// Cumulative Density Function
+    /// </summary>
+    public override double Cdf(double x) {
+      if (x < LeftBorder)
+        return 0;
+      else if (x >= RightBorder)
+        return 1;
+
+      return (Origin.Cdf(x) - m_CdfLeft) * m_Multiplicator;
+    }
+
+    /// <summary>
+    /// Probability Density Function
+    /// </summary>
+    public override double Pdf(double x) {
+      if (LeftBorder == RightBorder)
+        return x == LeftBorder ? double.PositiveInfinity : 0;
+
+      if (x < LeftBorder)
+        return 0.0;
+      else if (x > RightBorder)
+        return 0.0;
+      else
+        return Origin.Pdf(x) * m_Multiplicator;
+    }
+
+    /// <summary>
+    /// Quantile Function
+    /// </summary>
+    public override double Qdf(double x) {
+      if (x == 0)
+        return LeftBorder;
+      else if (x == 1)
+        return RightBorder;
+
+      return base.Qdf(x);
+    }
+
+    /// <summary>
+    /// To String
+    /// </summary>
+    public override string ToString() =>
+      $"{Origin} within [{LeftBorder}..{RightBorder}] range (proportional)";
+
+    #endregion Public
+  }
+
 }
