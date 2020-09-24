bd69b754abbd55758cfcba9ae3e2a03f3b21a9e2
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2020-09-24 10:50:50 +0300

    Linked List Extensions

---
+++ Gloson.Standard/Collections/Generic/Gloson.Collections.Generic.LinkedListExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gloson.Collections.Generic {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Linked List Extensions 
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static partial class LinkedListExtensions {
+    #region Public
+
+    /// <summary>
+    /// Add range after
+    /// </summary>
+    public static void AddRangeAfter<T>(this LinkedList<T> list, IEnumerable<T> value) {
+      if (null == list)
+        throw new ArgumentNullException(nameof(list));
+      else if (null == value)
+        throw new ArgumentNullException(nameof(value));
+
+      foreach (T item in value)
+        list.AddLast(item);
+    }
+
+    /// <summary>
+    /// Consume (as queue)
+    /// </summary>
+    public static IEnumerable<T> Consume<T>(this LinkedList<T> list) {
+      if (null == list)
+        throw new ArgumentNullException(nameof(list));
+
+      while (list.Count > 0) {
+        yield return list.First.Value;
+
+        list.RemoveFirst();
+      }
+    }
+
+    #endregion Public
+  }
+
+}
