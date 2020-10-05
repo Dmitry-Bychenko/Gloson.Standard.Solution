06c774b96ba8cc05599b609e425be929a30876f3
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2020-10-06 00:15:26 +0300

    Linq.Graphs

--- Gloson.Standard/Linq/Graphs/Gloson.Linq.Graphs.Basic.cs
+++ Gloson.Standard/Linq/Graphs/Gloson.Linq.Graphs.Basic.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace Gloson.Linq {
+namespace Gloson.Linq.Graphs {
   
   //-------------------------------------------------------------------------------------------------------------------
   //
