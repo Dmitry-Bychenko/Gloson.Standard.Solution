63d4a004e8dc88249b3f085eb143a84d4ba93b44
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-07-21 21:32:48 +0300

    Run Tasks

--- Gloson.Standard/Threading/Tasks/Gloson.Threading.Tasks.TaskExecution.cs
+++ Gloson.Standard/Threading/Tasks/Gloson.Threading.Tasks.TaskExecution.cs
@@ -18,11 +18,27 @@ namespace Gloson.Threading.Tasks {
     /// <summary>
     /// Run All Task in some sequence
     /// </summary>
-    public static async IAsyncEnumerable<Task<T>> RunAll<T>(IEnumerable<Task<T>> source) {
+    public static async IAsyncEnumerable<Task<T>> RunAll<T>(this IEnumerable<Task<T>> source) {
       if (source is null)
         throw new ArgumentNullException(nameof(source));
 
       for (var hs = new HashSet<Task<T>>(source); hs.Count > 0;) {
+        var completed = await Task.WhenAny(hs).ConfigureAwait(false);
+
+        hs.Remove(completed);
+
+        yield return completed;
+      }
+    }
+
+    /// <summary>
+    /// Run All Task in some sequence
+    /// </summary>
+    public static async IAsyncEnumerable<Task> RunAll(this IEnumerable<Task> source) {
+      if (source is null)
+        throw new ArgumentNullException(nameof(source));
+
+      for (var hs = new HashSet<Task>(source); hs.Count > 0;) {
         var completed = await Task.WhenAny(hs).ConfigureAwait(false);
 
         hs.Remove(completed);
