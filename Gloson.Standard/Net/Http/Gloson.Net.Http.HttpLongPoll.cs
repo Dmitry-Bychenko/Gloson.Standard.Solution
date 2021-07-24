5680d04d846e8b0b7bf6a1c0b9b4e256e6539162
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-07-24 20:33:50 +0300

    Long Poll with cancellation

--- Gloson.Standard/Net/Http/Gloson.Net.Http.HttpLongPoll.cs
+++ Gloson.Standard/Net/Http/Gloson.Net.Http.HttpLongPoll.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -23,7 +24,11 @@ namespace Gloson.Net.Http {
     /// <summary>
     /// Long Poll Read Lines
     /// </summary>
-    public static async IAsyncEnumerable<string> ReadLinesAsync(string address, string query, HttpMethod method) {
+    public static async IAsyncEnumerable<string> ReadLinesAsync(string address, 
+                                                                string query, 
+                                                                HttpMethod method,
+                                                                [EnumeratorCancellation]
+                                                                CancellationToken token = default) {
       using var http = new HttpClient();
 
       http.Timeout = Timeout.InfiniteTimeSpan;
@@ -38,27 +43,35 @@ namespace Gloson.Net.Http {
       };
 
       using var response = await http
-        .SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
+        .SendAsync(request, HttpCompletionOption.ResponseHeadersRead, token)
         .ConfigureAwait(false);
 
-      using var reader = new StreamReader(await response.Content.ReadAsStreamAsync().ConfigureAwait(false));
+      using var reader = new StreamReader(await response.Content.ReadAsStreamAsync(token).ConfigureAwait(false));
 
-      while (!reader.EndOfStream)
+      while (!reader.EndOfStream) {
+        token.ThrowIfCancellationRequested();
+
         yield return await reader.ReadLineAsync().ConfigureAwait(false);
+      }
     }
 
     /// <summary>
     /// Long Poll Read Lines
     /// </summary>
-    public static async IAsyncEnumerable<string> ReadLinesAsync(string address, string query) {
-      await foreach (var item in ReadLinesAsync(address, query, HttpMethod.Post).ConfigureAwait(false))
+    public static async IAsyncEnumerable<string> ReadLinesAsync(string address, 
+                                                                string query, 
+                                                                [EnumeratorCancellation]
+                                                                CancellationToken token = default) {
+      await foreach (var item in ReadLinesAsync(address, query, HttpMethod.Post, token).ConfigureAwait(false))
         yield return item;
     }
 
     /// <summary>
     /// Long Poll Read Lines
     /// </summary>
-    public static async IAsyncEnumerable<string> ReadLinesAsync(string address) {
+    public static async IAsyncEnumerable<string> ReadLinesAsync(string address,
+                                                                [EnumeratorCancellation]
+                                                                CancellationToken token = default) {
       await foreach (var item in ReadLinesAsync(address, "", HttpMethod.Get).ConfigureAwait(false))
         yield return item;
     }
