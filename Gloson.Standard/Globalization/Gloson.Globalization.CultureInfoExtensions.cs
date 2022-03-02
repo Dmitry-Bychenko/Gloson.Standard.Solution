d3a13295bbe9ba44514808896d147ec4e49094a1
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2022-03-02 08:41:24 +0300

    CultureInfo

---
+++ Gloson.Standard/Globalization/Gloson.Globalization.CultureInfoExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Gloson.Globalization {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Region Extensions
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static partial class CultureInfoExtensions {
+    #region Public
+
+    /// <summary>
+    /// To RegionInfo
+    /// </summary>
+    public static RegionInfo ToRegioninfo(this CultureInfo culture) =>
+      culture is null ? null : new RegionInfo(culture.LCID);
+
+    /// <summary>
+    /// To Flag Emoji 
+    /// </summary>
+    public static string ToFlagEmoji(this CultureInfo culture) =>
+      culture is null ? "🏳" : culture.ToRegioninfo().ToFlagEmoji();
+
+    #endregion Public
+  }
+
+}
---
+++ Gloson.Standard/Globalization/Gloson.Globalization.RegionInfoExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Linq;
+
+namespace Gloson.Globalization {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Region Extensions
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static partial class RegionInfoExtensions {
+    #region Public
+
+    /// <summary>
+    /// To Flag Emoji 
+    /// </summary>
+    public static string ToFlagEmoji(this RegionInfo region) {
+      if (region is null)
+        return "🏳";
+
+      var countryCode = region.ThreeLetterISORegionName;
+
+      return string.Concat(countryCode.ToUpper().Select(x => char.ConvertFromUtf32(x + 0x1F1A5)));
+    }
+
+    #endregion Public
+  }
+
+
+}
